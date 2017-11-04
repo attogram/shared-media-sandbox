32bcbdc7d93182e6f52d86c2847aeb4360f12bc0
Attogram Project <attogram@users.noreply.github.com>
2017-11-04 17:02:11 +0100

    psr

--- public/sandbox.php
+++ public/sandbox.php
@@ -13,22 +13,22 @@ $sandbox = new Sandbox();
 
 $sandbox->setTitle('shared-media-sandbox');
 
-$sandbox->setMethods([ 
-	// Class, Method, Arg, Identifiers
-	//['Class', 'method', 'query', false],
-]);
-	
-$sandbox->setVersions([
-	'Attogram\SharedMedia\Sandbox\Sandbox',
-	'Attogram\SharedMedia\Sandbox\Tools',
-	'Attogram\SharedMedia\Sandbox\Logger',
+$sandbox->setMethods([
+    // Class, Method, Arg, Identifiers
+    //['Class', 'method', 'query', false],
 ]);
 
-$sandbox->setVersionsPad(38);
+$sandbox->setVersions([
+    'Attogram\SharedMedia\Sandbox\Sandbox',
+    'Attogram\SharedMedia\Sandbox\Tools',
+    'Attogram\SharedMedia\Sandbox\Logger',
+]);
+
+$sandbox->setVersionsPad(36);
 
 $sandbox->setSources([
-	'commons'        => 'https://commons.wikimedia.org/w/api.php',
-	'en.wikipedia'   => 'https://en.wikipedia.org/w/api.php',
+    'commons'        => 'https://commons.wikimedia.org/w/api.php',
+    'en.wikipedia'   => 'https://en.wikipedia.org/w/api.php',
 ]);
 
 $sandbox->play();
